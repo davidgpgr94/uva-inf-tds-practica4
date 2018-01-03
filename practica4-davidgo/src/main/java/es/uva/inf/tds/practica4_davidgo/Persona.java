10e78c65b1da28ae90c4b420d91f41fa52405c0c
davidgo <david.gonzalez.perez@alumnos.uva.es>
2018-01-03 21:25:47 +0100

    Creado el stub del constructor y getters

--- practica4-davidgo/src/main/java/es/uva/inf/tds/practica4_davidgo/Persona.java
+++ practica4-davidgo/src/main/java/es/uva/inf/tds/practica4_davidgo/Persona.java
@@ -1,4 +1,27 @@
 package es.uva.inf.tds.practica4_davidgo;
 
 public class Persona {
+
+	public Persona(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Persona(String string, Persona[] conocidos) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Persona[] getAmigos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Persona[] getConocidos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getNombre() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
