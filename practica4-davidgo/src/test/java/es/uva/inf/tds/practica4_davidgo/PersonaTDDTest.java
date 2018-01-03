9c89f987d1715413a84fce2549fec5c56654154b
davidgo <david.gonzalez.perez@alumnos.uva.es>
2018-01-03 21:16:30 +0100

    Creando test TDD para la clase Persona

---
+++ practica4-davidgo/src/test/java/es/uva/inf/tds/practica4_davidgo/PersonaTDDTest.java
@@ -0,0 +1,14 @@
+package es.uva.inf.tds.practica4_davidgo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersonaTDDTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
