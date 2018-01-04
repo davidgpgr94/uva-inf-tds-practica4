2e3a00f0bde3a93eca9716bdeb7366214b7ee6e9
davidgo <david.gonzalez.perez@alumnos.uva.es>
2018-01-04 01:42:51 +0100

    Creada nueva Fixture con una persona con un amigo y un conocido

---
+++ practica4-davidgo/src/test/java/es/uva/inf/tds/practica4_davidgo/PersonaFixturePersonaConConocidosYAmigosTDDTest.java
@@ -0,0 +1,30 @@
+package es.uva.inf.tds.practica4_davidgo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PersonaFixturePersonaConConocidosYAmigosTDDTest {
+
+	private Persona p1, p2, p3;
+	private Persona[] conocidos;
+	
+	@Before
+	public void setUp() throws Exception {
+		//p1 considera amigo a p2
+		//p1 considera conocido a p3
+		p2 = new Persona("b");
+		p3 = new Persona("c");
+		Persona[] aux = {p2, p3};
+		conocidos = aux;
+		p1 = new Persona("a", conocidos);
+		p1.serAmigoDe(p2);
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
