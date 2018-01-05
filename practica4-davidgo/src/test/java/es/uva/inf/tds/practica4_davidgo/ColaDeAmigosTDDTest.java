dd4168ca2909a441742c3902562ca2d18e6212c1
davidgo <david.gonzalez.perez@alumnos.uva.es>
2018-01-05 20:53:01 +0100

    Generando stub de los métodos pedirVez, siguiente y colarA de la clase ColaDeAmigos

--- practica4-davidgo/src/test/java/es/uva/inf/tds/practica4_davidgo/ColaDeAmigosTDDTest.java
+++ practica4-davidgo/src/test/java/es/uva/inf/tds/practica4_davidgo/ColaDeAmigosTDDTest.java
@@ -19,5 +19,53 @@ public class ColaDeAmigosTDDTest {
 		fail("Obligado a fallar");
 		//TODO repasar una vez implementada la clase ColaDeAmigos
 	}
+	
+	@Test
+	public void testTDDpedirVez() {
+		Persona p1 = new Persona("a");
+		ColaDeAmigos c = new ColaDeAmigos();
+		c.pedirVez(p1, 0);
+		assertEquals(p1, c.siguiente());
+	}
 
+	@Test(expected = IllegalArgumentException.class)
+	public void testTDDpedirVezPersonaNull() {
+		Persona p1 = null;
+		ColaDeAmigos c = new ColaDeAmigos();
+		c.pedriVez(p1, 0);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testTDDpedirVezNamigosNegativo() {
+		Persona p1 = new Persona("a");
+		ColaDeAmigos c = new ColaDeAmigos();
+		c.pedirVez(p1, -1);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testTDDpedirVezParaMasDe10Amigos() {
+		Persona p1 = new Persona("a");
+		ColaDeAmigos c = new ColaDeAmigos();
+		c.pedirVez(p1, 11);
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void testTDDpedirVezDosVecesSinSerAtendido() {
+		Persona p1 = new Persona("a");
+		ColaDeAmigos c = new ColaDeAmigos();
+		c.pedirVez(p1, 0);
+		c.pedirVez(p1, 0);
+	}
+	
+	@Test(expected = IllegalStateException.class) 
+	public void testTDDpedirVezHabiendoSidoColado() {
+		Persona p2 = new Persona("b");
+		Persona[] conocidos = {p2};
+		Persona p1 = new Persona("a", conocidos);
+		p1.serAmigoDe(p2);
+		ColaDeAmigos c = new ColaDeAmigos();
+		c.pedirVez(p1, 1);
+		c.colarA(p2);
+		c.pedirVez(p2, 0);
+	}
 }
