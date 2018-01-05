8aeb30ab3d03ec72e4d6d9dad0d2a97056309e9f
davidgo <david.gonzalez.perez@alumnos.uva.es>
2018-01-05 20:34:22 +0100

    Añadido javadoc del constructor de ColaDeAmigos

--- practica4-davidgo/src/main/java/es/uva/inf/tds/practica4_davidgo/ColaDeAmigos.java
+++ practica4-davidgo/src/main/java/es/uva/inf/tds/practica4_davidgo/ColaDeAmigos.java
@@ -6,7 +6,12 @@ package es.uva.inf.tds.practica4_davidgo;
  *
  */
 public class ColaDeAmigos {
-	
+
+	/**
+	 * Crea un objeto ColaDeAmigos que representa una cola en la que se debe
+	 * pedir la vez para entrar y en la que se puede reservar sitio para un
+	 * grupo de amigos.
+	 */
 	public ColaDeAmigos() {
 		// TODO Auto-generated constructor stub
 	}
