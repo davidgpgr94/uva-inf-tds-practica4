62e06d3cd4ea4687851baf380112304748e0d1c0
davidgo <david.gonzalez.perez@alumnos.uva.es>
2018-01-05 20:17:48 +0100

    Creando test TDD para la clase ColaDeAmigos

---
+++ practica4-davidgo/src/test/java/es/uva/inf/tds/practica4_davidgo/ColaDeAmigosTDDTest.java
@@ -0,0 +1,19 @@
+package es.uva.inf.tds.practica4_davidgo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ColaDeAmigosTDDTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
