296c3f3380ce18c5b30d98035b8e0907002f190d
davidgo <david.gonzalez.perez@alumnos.uva.es>
2018-01-05 21:43:29 +0100

    Creados los test TDD para el método colarA

--- practica4-davidgo/src/test/java/es/uva/inf/tds/practica4_davidgo/ColaDeAmigosTDDTest.java
+++ practica4-davidgo/src/test/java/es/uva/inf/tds/practica4_davidgo/ColaDeAmigosTDDTest.java
@@ -67,4 +67,54 @@ public class ColaDeAmigosTDDTest {
 		c.colarA(p2);
 		c.pedirVez(p2, 0);
 	}
+	
+	@Test
+	public void testTDDcolarA() {
+		Persona p2 = new Persona("b");
+		Persona[] conocidos = {p2};
+		Persona p1 = new Persona("a", conocidos);
+		p1.serAmigoDe(p2);
+		ColaDeAmigos c = new ColaDeAmigos();
+		c.pedirVez(p1, 1);
+		c.colarA(p2);
+		assertEquals(p2, c.siguiente());
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testTDDcolarAPersonaNull() {
+		Persona p1 = new Persona("a");
+		ColaDeAmigos c = new ColaDeAmigos();
+		c.pedirVez(p1, 1);
+		c.colarA(null);
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void testTDDcolarAUnReservador() {
+		Persona p1 = new Persona("a");
+		ColaDeAmigos c = new ColaDeAmigos();
+		c.pedirVez(p1, 1);
+		c.colarA(p1);
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void testTDDcolarANoHayReservadorQueConsidereAmigoAlColado() {
+		Persona p2 = new Persona("b");
+		Persona[] conocidos = {p2};
+		Persona p1 = new Persona("a", conocidos);
+		ColaDeAmigos c = new ColaDeAmigos();
+		c.pedirVez(p1, 1);
+		c.colarA(p2);
+	}
+	
+	@Test(expected = IllegalStateException.class)
+	public void testTDDcolarAElReservadorQueConsideraComoAmigoAlColadoYaHaColadoAlLosAmigosParaLosQuePidioVez() {
+		Persona p2 = new Persona("b");
+		Persona p3 = new Persona("c");
+		Persona[] conocidos = {p2, p3};
+		Persona p1 = new Persona("a", conocidos);
+		ColaDeAmigos c = new ColaDeAmigos();
+		c.pedirVez(p1, 1);
+		c.colarA(p2);
+		c.colarA(p3);
+	}
 }
