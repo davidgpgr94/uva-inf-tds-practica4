7d25814a9e1cf4670373891abba76b59849530cd
davidgo <david.gonzalez.perez@alumnos.uva.es>
2018-01-06 00:21:38 +0100

    Preparando fixture con una ColaDeAmigos que ya tiene dos personas, de las cuales una ha reservado y otra ha sido colada por el primero. Además, el que ha reservado todavía puede colar a otro amigo

---
+++ practica4-davidgo/src/test/java/es/uva/inf/tds/practica4_davidgo/ColaDeAmigosFixtureColaConUnReservadorParaDosAmigosYUnoColado.java
@@ -0,0 +1,36 @@
+package es.uva.inf.tds.practica4_davidgo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * @author davidgo
+ *
+ */
+public class ColaDeAmigosFixtureColaConUnReservadorParaDosAmigosYUnoColado {
+	
+	private Persona p1, p2, p3;
+	private ColaDeAmigos cola;
+
+	@Before
+	public void setUp() throws Exception {
+		//en la cola estan p2 y luego p1 en este orden.
+		//p1 todavía puede colar a 1 amigo más
+		p2 = new Persona("b");
+		p3 = new Persona("c");
+		Persona[] conocidos = {p2, p3};
+		p1 = new Persona("a", conocidos);
+		cola = new ColaDeAmigos();
+		cola.pedirVez(p1, 2);
+		cola.colarA(p2);
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
