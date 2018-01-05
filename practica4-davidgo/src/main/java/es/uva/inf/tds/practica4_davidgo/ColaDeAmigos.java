30eb1f6ea2a43e02247d4f19b12eabad7364d1b3
davidgo <david.gonzalez.perez@alumnos.uva.es>
2018-01-05 21:03:35 +0100

    Añadido javadoc del método pedirVez de la clase ColaDeAmigos

--- practica4-davidgo/src/main/java/es/uva/inf/tds/practica4_davidgo/ColaDeAmigos.java
+++ practica4-davidgo/src/main/java/es/uva/inf/tds/practica4_davidgo/ColaDeAmigos.java
@@ -16,7 +16,24 @@ public class ColaDeAmigos {
 		// TODO Auto-generated constructor stub
 	}
 
-	public void pedirVez(Persona p1, int i) {
+	/**
+	 * Añade a reservador al final de la cola y reserva sitio delante de él para los nAmigos
+	 * 
+	 * @param reservador, Persona que pide la vez en la cola
+	 * @param nAmigos, número de amigos para los que reservador ha reservador sitio
+	 * 
+	 * @pre.condition {@code reservador != null}
+	 * @pre.condition {@code nAmigos >= 0 && nAmigos < 11}
+	 * @pre.condition {@code !esReservador(reservador)}
+	 * @pre.condition {@code !estaEnCola(reservador)}
+	 * @post.condition {@code esReservador(reservador)}
+	 * @post.condition {@code estaEnCola(reservador)}
+	 * @throws IllegalArgumentException si {@code reservador == null}
+	 * @throws IllegalArgumentException si {@code nAmigos < 0 || nAmigos > 10}
+	 * @throws IllegalStateException si {@code esReservador(reservador)}
+	 * @throws IllegalStateException si {@code estaEnCola(reservador)}
+	 */
+	public void pedirVez(Persona reservador, int nAmigos) {
 		// TODO Auto-generated method stub
 		
 	}
