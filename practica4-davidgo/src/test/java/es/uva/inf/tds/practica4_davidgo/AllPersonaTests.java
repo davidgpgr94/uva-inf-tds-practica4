32b51f2873af76adae32ffd59b75c5974775c072
davidgo <david.gonzalez.perez@alumnos.uva.es>
2018-01-07 03:16:46 +0100

    Creadas 3 test suits para ejecutar todos los test, los test de Persona, y los test de ColaDeAmigos

---
+++ practica4-davidgo/src/test/java/es/uva/inf/tds/practica4_davidgo/AllPersonaTests.java
@@ -0,0 +1,12 @@
+package es.uva.inf.tds.practica4_davidgo;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ PersonaCajaNegraTest.class, PersonaFixturePersonaConConocidosTDDTest.class,
+		PersonaFixturePersonaConConocidosYAmigosTDDTest.class, PersonaTDDTest.class })
+public class AllPersonaTests {
+
+}
